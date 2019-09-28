685e62c3142cde880077adcb630e2b6b909492e5
adhitamafikri <adhitamafikri@gmail.com>
2019-09-28 12:33:31 +0700

    can now delete item

---
+++ src/routes/api.js
@@ -0,0 +1,34 @@
+const { Router } = require('express')
+const Item = require('../models/Item')
+
+const router = Router()
+
+router.get('/', (req, res) => {
+  Item.find()
+    .then(items => res.json(items))
+    .catch(err => res.json(err))
+})
+
+router.post('/', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  })
+
+  newItem.save()
+    .then(item => res.json(item))
+})
+
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => {
+      item.remove()
+        .then(() => res.json({ message: 'Success deleted item' }))
+    })
+    .catch(err => {
+      res.status(404).json({
+        message: 'Something wrong'
+      })
+    })
+})
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -2,24 +2,27 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv/config')
+
+const apiRoutes = require('./routes/api')
 const config = require('./config')
+
 const app = express()
 
 // Middlewares
 app.use(bodyParser.json())
 
 // Connect DB
-mongoose.connect(
-  config.MONGODB_ATLAS_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  function() {
-    console.log('connected to DB')
-  }
+mongoose
+  .connect(config.MONGODB_ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected to DB'))
+  .catch((err) => console.log(err)
 )
 
-app.listen('4009', function() {
-  console.log('running on PORT 4009')
+// Use Routes
+app.use('/traversy-mern/v1', apiRoutes)
+
+// Listen
+const PORT = process.env.APP_PORT || 4009
+app.listen(PORT, function() {
+  console.log('running on PORT ', PORT)
 })
