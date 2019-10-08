e8109d60791df444e4ae51ffea49f42ac33e7a8d
adhitamafikri <adhitamafikri@gmail.com>
2019-10-08 17:18:54 +0700

    slight update

--- src/server.js
+++ src/server.js
@@ -31,13 +31,13 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
+} else {
+  // Listen
+  const PORT = process.env.APP_PORT || 4009
+  app.listen(PORT, function() {
+    console.log('running on PORT ', PORT)
+  })
 }
-
-// Listen
-// const PORT = process.env.APP_PORT || 4009
-// app.listen(PORT, function() {
-//   console.log('running on PORT ', PORT)
-// })
 
 module.exports = app
 module.exports.handler = serverless(app)
